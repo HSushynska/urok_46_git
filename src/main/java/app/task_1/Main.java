f56dce45e044f5366f1f8e401a43694471453f81
HSushynska <stoyanova.gv@gmail.com>
2025-01-29 10:39:21 +0100

    Task 2

--- src/main/java/app/task_1/Main.java
+++ src/main/java/app/task_1/Main.java
@@ -3,7 +3,7 @@ package app.task_1;
 public class Main {
     public static void main(String[] args) {
 
-        //Это кодб который решает задачу
+        //Это код, который решает задачу
         System.out.println("Task 1");
     }
 }
